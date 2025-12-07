d5c16d026b88a1b01ec1c53fcd936e8d8a09bb04
pravat-das01 <dpravatkumar0@gmail.com>
2025-12-07 12:36:54 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,6 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		int x=100;
+		String name="mike";
+		bollean isPresent=false;
+		int y=200;
+		char v = 'a';
+		double z =10.4;
+		float m =10.3f;
+		bollean isSub=true;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
